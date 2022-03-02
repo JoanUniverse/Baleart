1a00797b9bb5383c3796cc4b55cb3dc3795e6011
JoanUniverse <jonnyy184@gmail.com>
2022-03-02 18:39:03 +0100

    Comentaris App

--- baleart/src/App.js
+++ baleart/src/App.js
@@ -1,6 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
+// npm install react-router-dom
+// npm install react-bootstrap bootstrap
+// npm install axios
 
 function App() {
   return (
